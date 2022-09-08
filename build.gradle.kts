7f193ec1a8adc07be76e9d3b92e7dda1ee097b4b
BuildTools <unconfigured@null.spigotmc.org>
2022-09-08 16:22:49 +0900

    kalculation v1.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ plugins {
     id("maven-publish")
 }
 
-group = "nisstfast.github.io"
-version = "1.0.0"
+group = "nisstfast.github.com"
+version = "1.0.1"
 
 repositories {
     mavenCentral()
--- src/jvmMain/kotlin/com/github/nisstfast/kalculation/Kalculation/Kalculation.kt
+++ src/jvmMain/kotlin/com/github/nisstfast/kalculation/Kalculation/Kalculation.kt
@@ -1,11 +1,8 @@
-package io.github.nisstfast.kalculation.Kalculation
+package com.github.nisstfast.kalculation.Kalculation
 
 import java.util.*
 
-
-private val random = Random()
-
 fun RandomK(first: Int, second: Int): Int {
-    val second = second + 1
-    return random.nextInt(second - first) + first
+    val random = (first..second).random()
+    return random
 }
