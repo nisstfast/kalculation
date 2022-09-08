615bc50511c69bee0b81d00e000413f843466f9e
BuildTools <unconfigured@null.spigotmc.org>
2022-09-08 16:38:18 +0900

    kalculation v1.0.0

--- src/jvmMain/kotlin/com/github/nisstfast/Kalculation/Kalculation.kt
+++ src/jvmMain/kotlin/com/github/nisstfast/Kalculation/Kalculation.kt
@@ -1,7 +1,3 @@
-package com.github.nisstfast.kalculation.Kalculation
-
-import java.util.*
-
 fun RandomK(first: Int, second: Int): Int {
     val random = (first..second).random()
     return random
