53f35bfb5fee1e616a54804f50f3e211aeecb7ee
BuildTools <unconfigured@null.spigotmc.org>
2022-09-08 22:19:30 +0900

    kalculation v1.0.0

--- src/jvmMain/kotlin/com/github/nisstfast/kalculation/Kalculations.kt
+++ src/jvmMain/kotlin/com/github/nisstfast/kalculation/Kalculations.kt
@@ -4,3 +4,13 @@ fun RandomK(first: Long, second: Long) : Long {
     val RandomR = (first..second).random()
     return RandomR
 }
+
+fun PlusK(first: Long, second: Long) : Long {
+    val PlusR = first + second
+    return PlusR
+}
+
+fun MinusK(first: Long, second: Long) : Long {
+    val MinusR = first - second
+    return MinusR
+}
