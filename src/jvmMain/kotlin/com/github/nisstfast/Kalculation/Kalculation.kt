b46edbf714192ceaa311f613a6beeaa789ee72ff
BuildTools <unconfigured@null.spigotmc.org>
2022-09-08 17:17:45 +0900

    kalculation v1.0.0

--- src/jvmMain/kotlin/com/github/nisstfast/Kalculation/Kalculation.kt
+++
@@ -1,6 +0,0 @@
-package com.github.nisstfast.Kalculation
-
-fun RandomK(first: Long, second: Long): Long {
-    val random = (first..second).random()
-    return random
-}
---
+++ src/jvmMain/kotlin/com/github/nisstfast/kalculation/Kalculations.kt
@@ -0,0 +1,6 @@
+package com.github.nisstfast.kalculation
+
+fun RandomK(first: Long, second: Long) : Long {
+    val RandomR = (first..second).random()
+    return RandomR
+}
